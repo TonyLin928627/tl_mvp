d170b8316cd07db76b3bb5c8d434573cd80808fb
Tony Lin <tlin@smartspaceplc.com>
2020-10-13 14:15:45 +1300

    Created 3 empty screens

---
+++ app/src/main/java/io/tl/mvp/example/screens/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package io.tl.mvp.example.screens.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.tl.mvp.example.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.screen_home)
+    }
+}
